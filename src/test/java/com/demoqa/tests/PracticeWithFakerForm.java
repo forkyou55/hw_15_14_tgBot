1a9fdd18f07eb12b4c078a1b9ea123f38fc698e8
stanislavivanishchev <ivanishevstanislav@gmail.com>
2022-12-08 21:18:28 +0100

    added properties in test

--- src/test/java/com/demoqa/tests/PracticeWithFakerForm.java
+++ src/test/java/com/demoqa/tests/PracticeWithFakerForm.java
@@ -15,33 +15,33 @@ public class PracticeWithFakerForm extends com.demoqa.tests.TestFakerBase {
     @Story("Заполнение формы с фейковыми данными")
     @Test
     @DisplayName("Полное заполнение регистрационной формы c Faker")
-     void fillTestes() {
+    void fillTestes() {
         practiceFormPage.openPage()
-        .setFirstName(firstName)
-        .setLastName(lastName)
-        .setEmail(email)
-        .setGender(gender)
-        .setNumber(number)
-        .setBirthDate(day,month,year )
-        .setHobbies(hobby)
-        .setSubjects(subject)
-        .setUploadPicture("John Doe.jpeg")
-        .setAddress(address)
-        .setState(state)
-        .setCity(city)
-        .setSubmit();
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setNumber(number)
+                .setBirthDate(day, month, year)
+                .setHobbies(hobby)
+                .setSubjects(subject)
+                .setUploadPicture("John Doe.jpeg")
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .setSubmit();
 
         practiceFormPage.checkResultsTableVisible()
-                .checkResult("Student Name",firstName + " " + lastName)
-                .checkResult("Student Email",email)
-                .checkResult("Gender",gender)
-                .checkResult("Mobile",number)
-                .checkResult("Date of Birth",date)
-                .checkResult("Subjects",subject)
-                .checkResult("Hobbies",hobby)
-                .checkResult("Picture","John Doe.jpeg")
-                .checkResult("Address",address)
-                .checkResult("State and City",state + " " + city);
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", number)
+                .checkResult("Date of Birth", date)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobby)
+                .checkResult("Picture", "John Doe.jpeg")
+                .checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
 
     }
 }
